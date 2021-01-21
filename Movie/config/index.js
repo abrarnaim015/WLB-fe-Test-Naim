ff41cb02506c683c25520f2e755c35f9d4e7f284
abrarnaim015 <aabrarnaim24@gmail.com>
2021-01-21 20:19:49 +0700

    add mongo atlat BD

--- Movie/config/index.js
+++ Movie/config/index.js
@@ -1,8 +1,26 @@
-const databaseUrl = 'mongodb://localhost:27017'
-const { MongoClient, ObjectID } = require('mongodb')
-const client = new MongoClient(databaseUrl, { useUnifiedTopology: true })
-client.connect()
-const db = client.db('Mongo-Movies')
-const MoviesCollection = db.collection('movies')
+const { MongoClient } = require('mongodb')
 
-module.exports = { MoviesCollection, ObjectID }
+const databaseUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017'
+const client = new MongoClient(databaseUrl, { useUnifiedTopology: true })
+
+let db;
+
+const connect = cb => {
+  client.connect(err => {
+    if (err) {
+      console.log('connection failed')
+    } else {
+      console.log('connected')
+      db = client.db('Mongo-Movies')
+    }
+    cb(err)
+  })
+}
+
+const getDatabase = () => {
+  if (db) return db
+}
+// const MoviesCollection = getDatabase.collection('movies')
+// module.exports = { MoviesCollection, ObjectID, connect }
+
+module.exports = { getDatabase, connect }
--- Tv_Series/controller/Tv_Series-controller.js
+++ Tv_Series/controller/Tv_Series-controller.js
@@ -1,10 +1,14 @@
-const { TvSeriesCollection, ObjectID } = require('../config')
+const { getDatabase } = require('../config')
+const { ObjectID } = require('mongodb')
+// const { TvSeriesCollection, ObjectID } = require('../config')
 
 
 class TvSeriesController {
 
   static async getTvSeries(req, res, next) {
     try {
+      const db = getDatabase()
+      const TvSeriesCollection = db.collection('tvseries')
       const series = await TvSeriesCollection.find().toArray()
       res.status(200).json(series)
     } catch (err) {
@@ -15,6 +19,8 @@ class TvSeriesController {
   static async getTvSeriesById(req, res, next) {
     try {
       const id = req.params.id
+      const db = getDatabase()
+      const TvSeriesCollection = db.collection('tvseries')
       const getData = await TvSeriesCollection.findOne({ _id: ObjectID(id) })
       res.status(200).json(getData)
     } catch (err) {
@@ -25,6 +31,8 @@ class TvSeriesController {
   static async postTvSeries(req, res, next) {
     try {
       const dataBody = req.body
+      const db = getDatabase()
+      const TvSeriesCollection = db.collection('tvseries')
       const newSeries = await TvSeriesCollection.insertOne(dataBody)
       res.status(201).json(newSeries)
     } catch (err) {
@@ -36,6 +44,8 @@ class TvSeriesController {
     try {
       const id = req.params.id
       const dataBody = req.body
+      const db = getDatabase()
+      const TvSeriesCollection = db.collection('tvseries')
       const updateSeries = await TvSeriesCollection.findOneAndUpdate({ _id: ObjectID(id) }, { $set: { title: dataBody.title, overview: dataBody.overview, poster_path: dataBody.poster_path, popularity: dataBody.popularity, tags: dataBody.tags } }, { returnOriginal: false })
 
       res.status(200).json(updateSeries)
@@ -47,6 +57,8 @@ class TvSeriesController {
   static async deleteTvSeries(req, res, next) {
     try {
       const id = req.params.id
+      const db = getDatabase()
+      const TvSeriesCollection = db.collection('tvseries')
       const delSeries = await TvSeriesCollection.deleteOne({ _id: ObjectID(id) })
       if (delSeries.deletedCount === 1) {
         res.status(200).json({ msg: "Successfully deleted one document." })
